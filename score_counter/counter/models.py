17127f23f9719a031964c7316b1429308e826317
jactripper <skyway123@foxmail.com>
2017-06-26 17:01:54 +0800

    。。。

--- score_counter/counter/models.py
+++ score_counter/counter/models.py
@@ -116,10 +116,13 @@ class MajorScoreManager(models.Manager):
             MajorScore.objects.filter(id=item.id).update(grade=min(a), if_complete=com)
         pe_list = MajorScore.objects.filter(lesson_num='53000100',stu_num=stu_number)
         pe_grades = Score.objects.filter(number__startswith='530001',stu_num=stu_number)
-        if len(pe_grades)<= len(pe_list):
+        if len(pe_grades)< len(pe_list):
             for i in range(len(pe_grades)):
                 com = 1
-                id = pe_list[i].id
+                try:
+                    id = pe_list[i].id
+                except:
+                    continue
                 gra = pe_grades[i].grade
                 if gra=='F' or gra==0:
                     com = 0
@@ -128,7 +131,10 @@ class MajorScoreManager(models.Manager):
             pe_grades = sorted(pe_grades)
             for i in range(len(pe_list)):
                 com = 1
-                id = pe_list[i].id
+                try:
+                    id = pe_list[i].id
+                except:
+                    continue
                 gra = pe_grades[i].grade
                 if gra == 'F' or gra == 0:
                     com = 0
